b3cbac283d73fa11be9b3558c14308d78132c208
knowlet <knowlet3389@gmail.com>
2016-06-12 23:58:54 +0800

    Add RePassword Check and Hello in index

--- index.php
+++ index.php
@@ -32,10 +32,8 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 			<p>語言能力的提升，拓展字彙量是非常重要的一步。</p>
             <p>為了增加單字量，隨時複習是一個非常有效且普遍使用的做法。</p>
             <p>因此，我們提出這個單字卡系統，可以隨時使用單字卡背誦單字，這套系統也提供測驗系統，提供使用者隨時檢測自己的學習進度。</p>
-		</div>
-        <?php if (!(new Auth)->check()): ?>
-        <form method="POST">
-            <div class="container">
+            <?php if (!(new Auth)->check()): ?>
+            <form method="POST">
                 <div class="col c12">
                     <span class="addon">帳號</span><input type="text" class="smooth" name="user">
                 </div>
@@ -46,9 +44,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
                     <button type="subimt" class="btn btn-sm btn-b smooth">登入</button>
                     <a href="signup.php" class="btn btn-sm btn-a smooth">註冊</a>
                 </div>
-            </div>
-        </form>
-        <?php endif ?>
+            </form>
+            <?php else: ?>
+            <h1>Hello, <?=(new Session)->auth['username']?></h1>
+            <?php endif ?>
+        </div>
         <?php require_once 'layout/script.php' ?>
     </body>
 </html>
--- signup.php
+++ signup.php
@@ -1,16 +1,21 @@
 <?php
 require_once __DIR__.'/class/autoload.php';
-$ErrMsg = null;
+$Msg = null;
+$user = null;
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    if (isset($_POST['user']) && isset($_POST['pwd'])) {
+    if (isset($_POST['user']) && isset($_POST['pwd']) && isset($_POST['repwd'])) {
         try {
             $user = $_POST['user'];
             $pwd = $_POST['pwd'];
+            $repwd = $_POST['repwd'];
+            if ($pwd !== $repwd)
+                throw new Exception("兩次輸入的密碼不一樣");
             (new User)->createAccount($user, $pwd);
+            $Msg = "註冊成功！將自動跳轉至登入頁面...";
+            header("Refresh: 3; url=index.php");
         } catch (Exception $e) {
-            $ErrMsg = $e->getMessage();
+            $Msg = $e->getMessage();
         }
-
     }
 }
 
@@ -26,30 +31,28 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     </head>
     <body>
         <?php require_once 'layout/nav.php' ?>
-        <?php if (!is_null($ErrMsg)): ?>
-        <div class="msg"><?=$ErrMsg?></div>
+        <?php if (!is_null($Msg)): ?>
+        <div class="msg"><?=$Msg?></div>
         <?php endif ?>
         <div class="container">
-            <p>語言能力的提升，拓展字彙量是非常重要的一步。</p>
-            <p>為了增加單字量，隨時複習是一個非常有效且普遍使用的做法。</p>
-            <p>因此，我們提出這個單字卡系統，可以隨時使用單字卡背誦單字，這套系統也提供測驗系統，提供使用者隨時檢測自己的學習進度。</p>
-            <p>Sign Up</p>
-        </div>
-        <?php if (!(new Auth)->check()): ?>
-        <form method="POST" novalidate>
-            <div class="container">
+            <h1>Sign Up</h1>
+            <?php if (!(new Auth)->check()): ?>
+            <form method="POST">
                 <div class="col c12">
-                    <span class="addon">帳號</span><input type="text" class="smooth" name="user">
+                    <span class="addon">帳號</span><input type="text" class="smooth" name="user" value="<?=is_null($user)?'':$user?>">
                 </div>
                 <div class="col c12">
                     <span class="addon">密碼</span><input type="password" class="smooth" name="pwd">
                 </div>
                 <div class="col c12">
+                    <span class="addon">確認</span><input type="password" class="smooth" name="repwd">
+                </div>
+                <div class="col c12">
                     <button type="subimt" class="btn btn-sm btn-b smooth">註冊</button>
                     <a href="index.php" class="btn btn-sm btn-a smooth">登入</a>
                 </div>
-            </div>
-        </form>
-        <?php endif ?>
+            </form>
+            <?php endif ?>
+        </div>
     </body>
 </html>
